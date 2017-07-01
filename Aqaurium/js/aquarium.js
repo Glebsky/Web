262e02d68e4a1ddf5431bbbdc8160c8e05e70273
Glebsky <lotamountofsoul@gmail.com>
2017-07-01 18:54:35 +0300

    55

--- Aqaurium/js/aquarium.js
+++ Aqaurium/js/aquarium.js
@@ -1,10 +1,100 @@
 class fish{
-  constructor(){
-    this.direction = null;
-    this.image = null;
+  constructor(image){
+    this.HorDirection = 1;
+    this.VerDirection = null;
+    this.stopTimer = null;
+    this.image = image;
+    this.fish = document.createElement("div");
+    this.fish.classList.add("fish");
+    this.fish.style.top = Math.floor(Math.random()*window.innerHeight)+"px";
+    this.fish.style.left = Math.floor(Math.random()*window.innerWidth)+"px";
+
+    if (parseInt(this.fish.style.top) >= window.innerHeight - 32) {
+      this.fish.style.top = parseInt(this.fish.style.top)-32+"px";
+    }
+
+    if (parseInt(this.fish.style.left) >= window.innerWidth - 32) {
+      this.fish.style.left = parseInt(this.fish.style.left)-32+"px";
+    }
+
+    this.fish.style.backgroundImage = "url('"+image+"')";
+    document.body.appendChild(this.fish);
+
+  }
+  Direction(){
+    this.HorDirection = Math.floor(Math.random()*2);
+    this.VerDirection = Math.floor(Math.random()*4);
+    this.stopTimer = null;
   }
 
-  Move(){
-    
+ Move() {
+      if(this.HorDirection == 0){
+        if(this.fish.classList.contains("toRight")){this.fish.classList.remove("toRight");}
+        this.fish.classList.add("toLeft");
+        this.fish.style.left = parseInt(this.fish.style.left)-1+"px";
+        if(this.fish.style.left = parseInt(this.fish.style.left) <= 0){this.HorDirection = 1}
+      } else {
+        if(this.fish.classList.contains("toLeft")){this.fish.classList.remove("toLeft");}
+        this.fish.classList.add("toRight");
+        this.fish.style.left = parseInt(this.fish.style.left)+1+"px";
+        if(this.fish.style.left = parseInt(this.fish.style.left) >= window.innerWidth-32){this.HorDirection = 0}
+      }
+
+      if(this.VerDirection == 0){
+        this.fish.style.top = parseInt(this.fish.style.top)-1+"px";
+        if(this.fish.style.top = parseInt(this.fish.style.top) <= 0){this.VerDirection = 1}
+      } else if(this.VerDirection == 1) {
+        this.fish.style.top = parseInt(this.fish.style.top)+1+"px";
+        if(this.fish.style.top = parseInt(this.fish.style.top) >= window.innerHeight-32){this.VerDirection = 0}
+      }
+ }
+}
+var fishCount = 20;
+var FishArr = new Array();
+var fishImg;
+var TimerArr = new Array();
+//Create 10 fishes
+for (var i = 0; i < fishCount; i++) {
+  //get random value for image.
+  var fishImgRand =  Math.floor(Math.random()*4);
+  // Get random image
+  switch (fishImgRand) {
+    case 0:
+        fishImg = "images/fish-blue.png";
+      break;
+    case 1:
+        fishImg = "images/fish-green.png";
+        break;
+    case 2:
+        fishImg = "images/fish-red.png";
+        break;
+    case 3:
+        fishImg = "images/fish-yellow.png";
+        break;
+    default:
+        fishImg = "images/fish-red.png";
+  }
+  FishArr[i] = new fish(fishImg);
+}
+for (var i = 0; i < fishCount; i++) {
+  TimerArr[i] = null;
+}
+//set Interval Function;
+var rnd = Math.floor((Math.random()*10000)+3000);
+  setInterval(ChangeDirectionAll,rnd);
+  setInterval(MoveAll, 50);
+
+
+function MoveAll(){
+  for (var i = 0; i < fishCount; i++) {
+    FishArr[i].Move();
+  }
+}
+
+function ChangeDirectionAll(){
+  for (var i = 0; i < fishCount; i++) {
+    if (!FishArr[i].stopTimer) {
+        setTimeout(FishArr[i].Direction(), rnd);
+    }
   }
 }
